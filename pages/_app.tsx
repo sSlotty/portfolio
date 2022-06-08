485793ca0dcd59c0c0905b77310af3912a2cea51
Thanathip Ch <thanathip.oat@g.swu.ac.th>
2022-06-08 11:20:14 +0700

    change SEO and setup domain

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,10 +20,10 @@ function MyApp({ Component, pageProps }: AppProps) {
           url: "https://www.thanathip.com/",
           title: "Thanathip C. | Programer Developer",
           description:
-            "Hey! I'm Anurag, A Frontend Developer, Blogger and a Student!",
+            "Hey! I'm Thanathip Chanasri, A Backend Developer and a Student!",
           images: [
             {
-              url: "https://res.cloudinary.com/ddum5vpp3/image/upload/v1643532760/og-image_dwcwhp.png",
+              url: "https://scontent.fbkk2-4.fna.fbcdn.net/v/t39.30808-6/272868559_4808064765950420_7508818839187115985_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeF3lx-Q1zcbNuoc8jyu0pCChyO5kkrJGhmHI7mSSskaGbNJobe-dMZfFOvHfOjRVFjWmqwI7TsVzIyd8Jjshq9B&_nc_ohc=ZwdzxabXuDMAX_u5fLe&tn=NMNRNkIXZe-THIiH&_nc_ht=scontent.fbkk2-4.fna&oh=00_AT8gRMlGeVokcNZ6EU6eqDoQQ5R2z_8288YexxE-ymGaxA&oe=62A49E04",
               width: 800,
               height: 420,
               alt: "Thanathip C. | Programer Developer",
@@ -39,7 +39,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         ]}
       />
       <Head>
-        <link rel="icon" type="image/png" href="/assests/avatar.png" />
+        <link rel="icon" type="image/png" href="/assets/avatar.png" />
       </Head>
       <Component {...pageProps} />
 
